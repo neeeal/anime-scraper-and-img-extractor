586b0b7a83244544df06ff1a81deaed57360dba6
neeeal <matira.nealbartonjames@gmail.com>
2023-07-07 22:01:18 +0800

    update files

--- image_extractor.py
+++ image_extractor.py
@@ -1,4 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import pandas as pd
+
+urls_df = pd.read_csv('./urls_fixed.csv')
 
 driver = webdriver.Chrome('./chromedriver')
+
+driver.get(urls_df.values[0])
+print(driver.title)
