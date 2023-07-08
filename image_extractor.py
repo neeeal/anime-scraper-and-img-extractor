3fde6a7dec4599382dee2d5a031c94cdd5a880b4
neeeal <matira.nealbartonjames@gmail.com>
2023-07-08 22:30:33 +0800

    extractor and downloader py

--- image_extractor.py
+++ image_extractor.py
@@ -3,13 +3,13 @@ from selenium.webdriver.common.keys import Keys
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
-##from lxml import etree
+##from urllib.request import urlretrieve
 
 images = []
 
 urls_df = pd.read_csv('./urls_fixed.csv')
-driver = webdriver.Chrome()
 for i,url in enumerate(urls_df.values):
+    driver = webdriver.Chrome()
     driver.get(url[0])
     ##print(driver.title)
     current_url = url[0]
@@ -28,19 +28,28 @@ for i,url in enumerate(urls_df.values):
     ##print(soup.find("title").text)
 
     tables = soup.find_all(name='table')
-    try:
-        rows = tables[8].find_all(name='tr')[::2]
-        for row in rows:
-            img = row.find(name='img')
-            if img == None: continue
-            img_url = img.get('src')
-            final_img_url = 'https://www.getchu.com'+img_url[1:]
-            images.append(final_img_url)
-    except:
-        pass
-    if i>=1: break
+    for table in tables:
+        try:
+            if "キャ" in table.find_all("img")[0].get("alt"):
+                rows = table.find_all(name='tr')[::2]
+                for j,row in enumerate(rows):
+                    img = row.find(name='img')
+                    if img == None: continue
+                    img_url = img.get('src')
+                    final_img_url = 'https://www.getchu.com'+img_url[1:]
+##                    filename=path+"/"+str(i)+'_'+str(j)+'.jpeg'
+##                    urlretrieve(final_img_url,filename)
+                    images.append(final_img_url)
+        except Exception as e: print(e)
+
+    driver.close()
+    print(images)
+    if i>=3: break
 
 print(images)
+with open('image_urls.txt','w') as tfile:
+	tfile.write('\n'.join(images))
+	
 
 
 
