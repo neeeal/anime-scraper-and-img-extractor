268c4ddabfbd2af0842d678b7cd7bdcd701a556d
neeeal <matira.nealbartonjames@gmail.com>
2023-07-10 19:22:37 +0800

    updated main.py shorter code and exception catching

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ py_headers = ({'User-Agent':
 'Accept-Language': 'en-US, en;q=0.5'})
 
 ## Variable Declaration
-delay = 20
-total_time =0
+delay = 20      ## Time to wait for element after page load
+total_time = 0  ## Total execution time counter
 
 def load_data(filepath):
     ## Loading URLS from csv
@@ -27,19 +27,22 @@ def get_current_url(url,driver):
     ## Bypassing age checking on to actual webpage
     ## Checking if page will load
     driver=driver; driver.get(url)
-    try: myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located(('xpath','//*[@id="wrapper"]/div[7]/table/tbody/tr/td[1]/span/a')))
+    try: myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located(('xpath',
+                        '//*[@id="wrapper"]/div[7]/table/tbody/tr/td[1]/span/a')))
     except TimeoutException: print("Loading took too much time!"); driver.refresh()
     try:
-        confirm = driver.find_element('xpath','//*[@id="wrapper"]/div[7]/table/tbody/tr/td[1]/span/a')
+        confirm = driver.find_element('xpath',
+                        '//*[@id="wrapper"]/div[7]/table/tbody/tr/td[1]/span/a')
         confirm.click()
         current_url = driver.current_url
-    except: pass  
+    except Exception as e: pass  
     return current_url
 
 def write_to_file(filename,container_num):
     ## Writing image to file
     with open(f'./images/{filename}', 'wb') as file:
-        file.write(driver.find_element('xpath',f'//*[@id="wrapper"]/table[4]/tbody/tr[{container_num}]/td[1]/img').screenshot_as_png)
+        file.write(driver.find_element('xpath',
+            f'//*[@id="wrapper"]/table[4]/tbody/tr[{container_num}]/td[1]/img').screenshot_as_png)
         file.close()
     pass
 
@@ -76,77 +79,10 @@ def Scrape(url,driver):
 ## MAIN
 if __name__ == '__main__':
     urls = load_data('./urls_fixed.csv')
-    for i,url in enumerate(urls):
+    for i,url in enumerate(urls[:]):
         driver = webdriver.Chrome()
         total_time += Scrape(url,driver)
         driver.close()
         if i>=7: break ## Tester Only. Comment for full run.
     print("\nTotal runtime --- %.2f minutes ---" % (total_time/60))
 
-
-
-
-
-########
-########
-
-##    images = []
-##    total_time = 0.0
-##    delay = 20 # seconds
-##    urls_df = pd.read_csv('./urls_fixed.csv')
-##    for i,url in enumerate(urls_df.values):
-##        start_time = time.time()
-##        print("Getting from:",url[0], end=" ")
-##        driver = webdriver.Chrome()
-##        driver.get(url)
-##        try:
-##            myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located(('xpath','//*[@id="wrapper"]/div[7]/table/tbody/tr/td[1]/span/a')))
-##    ##        print("Page is ready!")
-##        except TimeoutException:
-##            print("Loading took too much time!")
-##        current_url = url
-##        try:
-##            confirm = driver.find_element('xpath','//*[@id="wrapper"]/div[7]/table/tbody/tr/td[1]/span/a')
-##            confirm.click()
-##            current_url = driver.current_url
-##        except:
-##            pass
-##
-##        webpage = requests.get(current_url, headers = py_headers)
-##        soup = BeautifulSoup(webpage.content, "html.parser")
-##
-##        tables = soup.find_all(name='table')
-##        for table in tables:
-##            try:
-##                if "キャ" in table.find_all("img")[0].get("alt"):
-##                    rows = table.find_all(name='tr')[::2]
-##                    k=1
-##                    l=0
-##                    for j,row in enumerate(rows):
-##                        img = row.find(name='img')
-##                        if img == None: continue                     
-##                        img_url = img.get('src')
-##    ##                    final_img_url = 'https://www.getchu.com'+img_url[1:]
-##                        filename = img_url[1:].split("/")[-1].replace("jpg","png")
-##                        with open(f'./images/{filename}', 'wb') as file:
-##                            file.write(driver.find_element('xpath',f'//*[@id="wrapper"]/table[4]/tbody/tr[{k}]/td[1]/img').screenshot_as_png)
-##                        file.close()
-##                        k+=2; l+=1
-##    ##                    filename=path+"/"+str(i)+'_'+str(j)+'.jpeg'
-##    ##                    urlretrieve(final_img_url,filename)
-##    ##                    images.append(final_img_url)
-##            except Exception as e:
-##                continue
-##            
-##        print("Successfully retrieved",l,"images", end="    ")
-##        driver.close()
-##        end_time = time.time() - start_time
-##        print("--- %s seconds ---" % (end_time))
-##        total_time += end_time
-##        if i>=1: m=i; break
-##
-##        
-##    print("\nTotal Execution Time --- %s minutes ---" % (total_time/60))
-##    print("Average Time per URL --- %s seconds ---" % (total_time/m))
-##
-##
